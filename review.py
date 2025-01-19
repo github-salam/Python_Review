c0cf156e204f346054ee4e3a252e2911fdbf611f
abdus-salam <abdussalam0419@gmail.com>
2025-01-19 16:04:08 +0600

    Till variable

---
+++ review.py
@@ -0,0 +1,53 @@
+# getting started
+#print("hello world")
+
+# system modile
+# import sys
+# print(sys.version)
+
+# indentation
+# if 6 > 5:
+#     print("6 is greater than 5")  # this line containing some white space at the beggining which is consider as indentation
+
+# variable declaration -> when you assign value to it
+# var = 59
+# print(var)
+
+# comment is already used once
+
+"""
+this 
+could 
+be used as multiline comment
+"""
+"""
+type_casting = str(90)
+print(type(type_casting))
+"""
+
+# x,y,z = "salam", 45, "kalam" # assgining multiple values in a single line
+# print(x,y,z)
+
+# x = y = z = "same values to multile variables\n"
+# print(x,y,z)
+
+# unpack values
+# li = [23,45,67,89,"kuttish", "motu"]
+
+# a,b,c,d,e,f = li
+# print(a,b,c,d,e,f)
+
+# create outside of a function is known as global variable
+# k = 5
+# def example():
+#     k = "updated"
+#     print(f"i am testing global variable k is {k}")
+# example()
+# print(f"gloabl {k}")
+
+fahim = 7
+def key():
+    global fahim
+    fahim = 6
+key()
+print(fahim)
